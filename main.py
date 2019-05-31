29975f45e47719dddcdda387d9cc887a4af23547
Anastasia <ivanovaa.anastasia@gmail.com>
2019-05-31 23:59:37 +0300

    add a little bit comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ passwd = 'openwaypassword'
 
 
 def is_el_displayed(element) -> None:
+    # raise error if there isn't any element
     if element.is_displayed():
         return
     raise Exception('No element is displayed')
@@ -16,6 +17,7 @@ def press_button(class_name: str, num: int = 1) -> None:
     btn = browser.find_elements_by_class_name(class_name)[num]
     is_el_displayed(btn)
     btn.click()
+    # for waiting page load - 5 seconds
     sleep(5)
 
 
@@ -27,6 +29,7 @@ def paste_value_in_input_filed(filed: str, value: str) -> None:
 
 
 def is_letter_unread(letter) -> bool:
+    # check a marker of unread letter
     unread_flags = letter.find_elements_by_class_name('state_toRead')
     return len(unread_flags) != 0
 
@@ -35,6 +38,7 @@ def get_unread_messages() -> str:
     for letter in browser.find_elements_by_class_name('ns-view-messages-item-wrap'):
         if is_letter_unread(letter):
             letter_title = letter.find_element_by_class_name('js-message-snippet-text').text
+            # take a first string from text of this element - it's a title of letter
             return letter_title.split('\n')[0]
     raise Exception('No unread messages')
 
